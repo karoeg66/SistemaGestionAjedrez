87d9f7fc5aace1890572c852a9da609bb2be3ad2
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-27 12:48:46 -0500

    Se suben cambios a gestor de partida y form de partida

--- src/main/java/org/karo/eg66/GestionPartida.java
+++ src/main/java/org/karo/eg66/GestionPartida.java
@@ -1,13 +1,27 @@
 package org.karo.eg66;
 
-import java.sql.ResultSet;
-
 public class GestionPartida {
     Resultado resultadoJugador1;
     Resultado resultadoJugador2;
     Partida partida;
+    PantallaPartida pantallaPartida;
 
     public GestionPartida(Partida partida) {
         this.partida = partida;
+    }
+    public void otorgarPuntos () {
+        if (resultadoJugador1 == Resultado.VICTORIA) {
+            partida.jugador1.setPuntaje(partida.jugador1.getPuntaje() + 20);
+        }
+        else {
+            partida.jugador1.setPuntaje(partida.jugador1.getPuntaje() - 10);
+        }
+        if (resultadoJugador2 == Resultado.VICTORIA) {
+            partida.jugador2.setPuntaje(partida.jugador2.getPuntaje() + 20);
+        }
+        else {
+            partida.jugador2.setPuntaje(partida.jugador2.getPuntaje() - 10);
+        }
+
     }
 }
--- src/main/java/org/karo/eg66/PantallaPartida.java
+++ src/main/java/org/karo/eg66/PantallaPartida.java
@@ -4,4 +4,17 @@ import javax.swing.*;
 
 public class PantallaPartida {
     private JLabel lblResultadosPartida;
+    private JComboBox cboxResultado;
+    private JLabel lblPuntosCalidad;
+    private JTextField txtPuntosJugador1;
+    private JTextField txtPuntosJugador2;
+    private JLabel lblJugador1;
+    private JLabel lblJugador2;
+    private JLabel lblGanador;
+    private JButton btnAceptar;
+    GestionPartida gestionPartida;
+
+    public PantallaPartida() {
+    }
+
 }
