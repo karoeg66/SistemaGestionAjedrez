d5d4784da33a3ccaf773c800f543cc9e9709dcc2
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-27 10:50:43 -0500

    Se crea la clase nodo para la cola

--- src/main/java/org/karo/eg66/NodoCola.java
+++ src/main/java/org/karo/eg66/NodoCola.java
@@ -1,4 +1,26 @@
 package org.karo.eg66;
 
+import javax.print.attribute.standard.JobOriginatingUserName;
+
 public class NodoCola {
+    private Jugador jugador;
+    private NodoCola siguiente;
+
+    public NodoCola(Jugador jugador) {
+        this.jugador = jugador;
+        this.siguiente = null;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+    public NodoCola getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(NodoCola siguiente) {
+        this.siguiente = siguiente;
+    }
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
 }
