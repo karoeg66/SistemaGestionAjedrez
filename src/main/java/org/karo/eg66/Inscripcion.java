261ab207d57b99d9d1a1c63950ce5a262afdd44e
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-27 16:26:48 -0500

    Se realiza la logica base de la clase inscripcion

---
+++ src/main/java/org/karo/eg66/Inscripcion.java
@@ -0,0 +1,55 @@
+package org.karo.eg66;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Inscripcion {
+    private JTextField txtNombre;
+    private JTextField txtCedula;
+    private JLabel lblNombre;
+    private JLabel lblCedula;
+    private JButton btnRegistrar;
+    Jugador jugador;
+    Cola cola;
+
+    public Inscripcion(Cola cola) {
+        this.cola = cola;
+
+        btnRegistrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String nombre = txtNombre.getText().trim();
+                String cedula = txtCedula.getText().trim();
+
+                if (!comprobarCampo(nombre) || !comprobarCampo(cedula)) {
+                    JOptionPane.showMessageDialog(null, "Error: Todos los campos son obligatorios.", "Campos vacíos", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+
+
+                if (!comprobarCedulaNumerica(cedula)) {
+                    JOptionPane.showMessageDialog(null, "Error: La cedula debe contener unicamente numeros.", "Error de formato", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+
+                jugador = new Jugador(nombre, cedula);
+                cola.enqueue(jugador);
+
+                JOptionPane.showMessageDialog(null, "Jugador inscrito con exito en la cola");
+                txtNombre.setText("");
+                txtCedula.setText("");
+            }
+        });
+    }
+
+    public boolean comprobarCampo(String campo) {
+        return campo != null && !campo.trim().isEmpty();
+    }
+
+    public boolean comprobarCedulaNumerica(String cedula) {
+        return cedula.matches("\\d+");
+    }
+}
