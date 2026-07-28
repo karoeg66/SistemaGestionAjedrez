653cda452f20bc03a0ee8841fdc216915133ebd3
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-28 13:20:16 -0500

    Se añade el metodo to string en partida

--- src/main/java/org/karo/eg66/Partida.java
+++ src/main/java/org/karo/eg66/Partida.java
@@ -1,6 +1,7 @@
 package org.karo.eg66;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Partida {
     int contador = 1;
@@ -9,11 +10,16 @@ public class Partida {
     Jugador jugador1;
     Jugador jugador2;
     String resultado;
+    DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
     public Partida(Jugador jugador1, Jugador jugador2) {
         this.id = contador++;
         this.fecha = LocalDate.now();
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
+    }
+
+    public String toString() {
+        return "Fecha: " + fecha.format(formateador) + "\n Jugador1: " + jugador1 + "\nJugador2: " + jugador2 + "\n ----------------------------------" ;
     }
 }
