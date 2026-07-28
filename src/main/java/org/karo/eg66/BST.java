2b35023f852e5c54a04719b8e9c0973196fbf6d3
JuanPFrancoL <juanp.franco@autonoma.edu.co>
2026-07-28 17:05:02 -0500

    Metodo search adaptado

--- src/main/java/org/karo/eg66/BST.java
+++ src/main/java/org/karo/eg66/BST.java
@@ -5,28 +5,45 @@ public class BST {
 
     public BST(){
         this.raiz = null;
-    }}
+    }
+
+    public NodoBST insert(NodoBST nodo, Jugador jugador){
+        if(nodo == null){
+            return new NodoBST(jugador);
+        }
+        if(jugador.getPuntaje() < nodo.jugador.getPuntaje()){
+            nodo.izquierda = insert(nodo.izquierda, jugador);
+        }
+        else if(jugador.getPuntaje() > nodo.jugador.getPuntaje()){
+            nodo.derecha = insert(nodo.derecha, jugador);
+        }
+        else{
+            //Desempate
+            if(jugador.getDerrotas() < nodo.jugador.getDerrotas()){
+                nodo.izquierda = insert(nodo.izquierda, jugador);
+            }
+            else if(jugador.getDerrotas() > nodo.jugador.getDerrotas()){
+                nodo.derecha = insert(nodo.derecha, jugador);
+            }
+            else{
+                if(Integer.parseInt(jugador.getId()) < Integer.parseInt(nodo.jugador.getId())){
+                    nodo.izquierda = insert(nodo.izquierda, jugador);
+                }
+                else{
+                    nodo.derecha = insert(nodo.derecha, jugador);
+                }
+            }
+        }
+        return nodo;
+    }
+
+    public boolean search(NodoBST nodo, String id){
+        if(nodo == null) return false;
+        if(id == nodo.jugador.getId()) return true;
+        return search(nodo.izquierda, id) || search(nodo.derecha, id);
+    }
+//    public NodoBST delete(NodoBST nodo, String id){
 //
-//    public NodoBST insert(NodoBST nodo, Jugador jugador){
-//        if(nodo == null){
-//            return new NodoBST(jugador);
-//        }
-//        if(jugador.getPuntaje() < nodo.jugador.getPuntaje()){
-//            nodo.izquierda = insert(nodo.izquierda, jugador);
-//        }
-//        else if(jugador.getPuntaje() < nodo.jugador.getPuntaje()){
-//            nodo.derecha = insert(nodo.derecha, jugador);
-//        }
-//        else{
-//            //Desempate
-//            if(jugador.getJugadasDestacadas() < nodo.jugador.getJugadasDestacadas()){
-//                nodo.izquierda = insert(nodo.izquierda, jugador);
-//            }
-//            else{
-//                nodo.derecha = insert(nodo.derecha, jugador);
-//            }
-//        }
-//        return nodo;
 //    }
-//
-//}
+
+}
